5f2dc507cc024d1619e8af1bf4e92fb7fea64982
prvladko <prvladko@gmail.com>
2021-09-26 22:42:57 +0300

    in each iteration add pair name, contract name to the list 'contracts'

--- binance_futures.py
+++ binance_futures.py
@@ -7,9 +7,12 @@ logger = logging.getLogger()
 def get_contracts():
     # response_object = requests.get('https://testnet.binancefuture.com/fapi/v1/exchangeInfo)  # for test
     response_object = requests.get('https://fapi.binance.com/fapi/v1/exchangeInfo')  # from https://binance-docs.github.io/apidocs/futures/en/#check-server-time
-    print(response_object.status_code)
+
+    contracts = []
 
     for contract in response_object.json()['symbols']:
-        print(contract['pair'])
+        contracts.append(contract['pair'])
 
-get_contracts()
+    return contracts
+
+print(get_contracts())
