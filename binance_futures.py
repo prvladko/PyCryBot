ff89bef3a61e8fa5ee237d7ebc0aa572ccce1961
prvladko <prvladko@gmail.com>
2021-09-26 22:20:53 +0300

    get_contracts func added

--- binance_futures.py
+++ binance_futures.py
@@ -1,5 +1,11 @@
 import logging
 import requests
 
-
 logger = logging.getLogger()
+
+def get_contracts():
+    # for test 'https://testnet.binancefuture.com'
+    response_object = requests.get('https://fapi.binance.com/fapi/v1/exchangeInfo')  # from https://binance-docs.github.io/apidocs/futures/en/#check-server-time
+    print(response_object.status_code, response_object.json())
+
+get_contracts()
