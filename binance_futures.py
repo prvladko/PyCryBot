19e6f3fec677406531e77575ae9b70f59eedb13e
prvladko <prvladko@gmail.com>
2021-09-26 21:59:35 +0300

    if __name__ == 'main': statement added

--- binance_futures.py
+++ binance_futures.py
@@ -1,7 +1,3 @@
 import logging
 
 logger = logging.getLogger()
-
-# func to output some log
-def write_log():
-    logger.info('Hello from the Binance connector')
--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import tkinter as tk
 import logging
-from binance_futures import write_log
 
 logger = logging.getLogger()
 
@@ -18,12 +17,6 @@ file_handler.setLevel(logging.DEBUG)
 logger.addHandler(stream_handler)  # add stream_handler to the logger instance
 logger.addHandler(file_handler)
 
-logger.debug('this message is important only when debugging the program')
-logger.info('this message just shows basic information')
-logger.warning('this message is about something you should pay attention to')
-logger.error('this message helps to debug an error that occurred in program')
-
-write_log()
-
-root = tk.Tk()  # main window of the bot
-root.mainloop()  # blocking func that prevent program from terminating ('event loop' func - wait for action from user)
+if __name__ == 'main':  # statement will be executed only if the main module will be executed
+    root = tk.Tk()  # main window of the bot
+    root.mainloop()  # blocking func that prevent program from terminating ('event loop' func - wait for action from user)
