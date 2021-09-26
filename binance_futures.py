b48d88f5047a576a98f96a2367458f7f70adabff
prvladko <prvladko@gmail.com>
2021-09-26 22:39:27 +0300

    get all symbol pairs

--- binance_futures.py
+++ binance_futures.py
@@ -8,6 +8,8 @@ def get_contracts():
     # response_object = requests.get('https://testnet.binancefuture.com/fapi/v1/exchangeInfo)  # for test
     response_object = requests.get('https://fapi.binance.com/fapi/v1/exchangeInfo')  # from https://binance-docs.github.io/apidocs/futures/en/#check-server-time
     print(response_object.status_code)
-    pprint.pprint(response_object.json())
+
+    for contract in response_object.json()['symbols']:
+        print(contract['pair'])
 
 get_contracts()
