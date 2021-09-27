b29c6d77a9e106741055ddf73b5a75fe9bb6bcab
prvladko <prvladko@gmail.com>
2021-09-27 21:46:57 +0300

    add bitmex_futures.py

--- bitmex_futures.py
+++ bitmex_futures.py
@@ -5,8 +5,8 @@ import pprint
 logger = logging.getLogger()
 
 def get_contracts():
-    # response_object = requests.get('https://testnet.binancefuture.com/fapi/v1/exchangeInfo)  # for test
-    response_object = requests.get('https://fapi.binance.com/fapi/v1/exchangeInfo')  # from https://binance-docs.github.io/apidocs/futures/en/#check-server-time
+    # response_object = requests.get('https://testnet.bitmex.com/api/v1/instrument')  # for test
+    response_object = requests.get('https://testnet.bitmex.com/api/v1/instrument')  # from https://binance-docs.github.io/apidocs/futures/en/#check-server-time
 
     contracts = []
 
