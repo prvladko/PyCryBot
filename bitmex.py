e6efbe7c2358a73dbbd5fbdb25dba514853515fa
prvladko <prvladko@gmail.com>
2021-09-27 22:41:52 +0300

    get Bitmex pairs Done!!! "binance_futures.py" now don't need

--- bitmex.py
+++ bitmex.py
@@ -3,4 +3,11 @@ import requests
 def get_contracts():
     contracts = []
 
-    response_object = requests.get('')
+    response_object = requests.get('https://bitmex.com/api/v1/instrument/active')
+
+    for contract in response_object.json():
+        contracts.append(contract['symbol'])
+
+    return contracts
+
+print(get_contracts())
