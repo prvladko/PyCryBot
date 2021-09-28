c10019948763207fc1746ec19863773082fda0d3
prvladko <prvladko@gmail.com>
2021-09-28 14:25:35 +0300

    try to solve the problem (with new param 'symbol')

--- bitmex_futures.py
+++ bitmex_futures.py
@@ -5,16 +5,13 @@ import pprint
 logger = logging.getLogger()
 
 def get_contracts():
-    # response_object = requests.get('https://testnet.bitmex.com/api/v1/instrument')  # for test
-    response_object = requests.get('https://testnet.bitmex.com/api/v1/instrument/active')  # from https://binance-docs.github.io/apidocs/futures/en/#check-server-time
-    #print(response_object.status_code)
-    #pprint.pprint(response_object.json())
+    contracts = []
 
-   contracts = []
+    response_object = requests.get('https://bitmex.com/api/v1/instrument/active')
 
     for contract in response_object.json():
         contracts.append(contract['symbol'])
 
     return contracts
 
-get_contracts()
+print(get_contracts())
