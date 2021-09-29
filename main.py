fd6610c6d9b08a7c1e36ceb8b8b89afdc394485c
prvladko <prvladko@gmail.com>
2021-09-29 23:29:30 +0300

    grid method

--- main.py
+++ main.py
@@ -25,12 +25,12 @@ if __name__ == '__main__':  # statement will be executed only if the main module
 
     root = tk.Tk()  # main window of the bot
 
-
+    i = 0  # need for .grid method (first widget will be placed on the first row)
 
     for contract in binance_contracts:
         label_widget = tk.Label(root, text=contract)
-        label_widget.pack(side=tk.LEFT)  # TOP,BOTTOM,LEFT,RIGHT # .pack method places widgets relatively to each other
-        #label_widget.grid(side=tk.BOTTOM)  # .grid method can specify the column and row number of each widget
-
+        #label_widget.pack(side=tk.LEFT)  # TOP,BOTTOM,LEFT,RIGHT # .pack method places widgets relatively to each other
+        label_widget.grid(row=i, column=0)  # .grid method can specify the column and row number of each widget
+        i += 1
 
     root.mainloop()  # blocking func that prevent program from terminating ('event loop' func - wait for action from user)
