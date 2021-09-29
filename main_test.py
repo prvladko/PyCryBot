8178728b3ea79ec709861adfad3ef91f729f5da0
prvladko <prvladko@gmail.com>
2021-09-29 22:09:12 +0300

    WELL DONE! it was just to remove block "if __name__ == 'main_test':"

--- main_test.py
+++ main_test.py
@@ -1,23 +1,23 @@
 import tkinter as tk
 from bitmex import get_contracts
 
-if __name__ == 'main':  # statement will be executed only if the main module will be executed
+#if __name__ == 'main_test':  # statement will be executed only if the main module will be executed
 
-    bitmex_contracts = get_contracts()
+bitmex_contracts = get_contracts()
 
-    root = tk.Tk()  # main window of the bot
+root = tk.Tk()  # main window of the bot
 
-    #i = 0
+i = 0
 
-    for contract in binance_contracts:
-        label_widget = tk.Label(root, text=contract)
-        label_widget.grid(row=i, column=0)
+for contract in bitmex_contracts:
+    label_widget = tk.Label(root, text=contract)
+    label_widget.grid(row=i, column=0)
 
-        #i += 1
-
-
+    i += 1
 
 
 
-    #root = tk.Tk()
-    root.mainloop()
+
+
+#root = tk.Tk()
+root.mainloop()
