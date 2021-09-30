7061a0ceccb2c7d18c195de15f0848dfa088bacf
prvladko <prvladko@gmail.com>
2021-09-30 22:44:55 +0300

    create class BinanceFuturesClient

--- connectors/binance_futures.py
+++ connectors/binance_futures.py
@@ -1,18 +1,13 @@
 import logging
 import requests
-import pprint
 
 logger = logging.getLogger()
 
-def get_contracts():
-    # response_object = requests.get('https://testnet.binancefuture.com/fapi/v1/exchangeInfo)  # for test
-    response_object = requests.get('https://fapi.binance.com/fapi/v1/exchangeInfo')  # from https://binance-docs.github.io/apidocs/futures/en/#check-server-time
+class BinanceFuturesClient:
+    def __init__(self, testnet):
+        if testnet:
+            self.base_url = 'https://testnet.binancefuture.com'
+        else:
+            self.base_url = 'https://fapi.binance.com'
 
-    contracts = []
-
-    for contract in response_object.json()['symbols']:
-        contracts.append(contract['pair'])
-
-    return contracts
-
-print(get_contracts())
+        logger.info('Binance Futures Client is successfully initialized')
