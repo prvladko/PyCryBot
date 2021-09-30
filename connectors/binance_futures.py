3093e0ec0188ab95fa6a12822479bb4389e2fc53
prvladko <prvladko@gmail.com>
2021-09-30 23:00:17 +0300

    define make_request method to simplify and make code clear

--- connectors/binance_futures.py
+++ connectors/binance_futures.py
@@ -12,11 +12,27 @@ class BinanceFuturesClient:
 
         logger.info('Binance Futures Client is successfully initialized')
 
+    def make_request(self, method, endpoint, data):
+        if method == 'GET':
+            response = requests.get(self.base_url + endpoint, params=data)
+        else:
+            raise ValueError()
+
+        if response.status_code == '200':
+            return response.json()
+        else:
+            logger.error('Error while making %s request to %s: %s (error code %s)',
+                         method, endpoint, response.json(), response.status_code)
+            return None
+
     def get_contracts(self):
+        requests.get()
         return
 
     def get_historical_candles(self):
+        requests.get()
         return
 
     def get_bid_ask(self):
+        requests.get()
         return
