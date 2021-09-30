0425f426d9f476adb12629e1c2d1fc31fd11565b
prvladko <prvladko@gmail.com>
2021-09-30 23:35:34 +0300

    define get_historical_candles method

--- connectors/binance_futures.py
+++ connectors/binance_futures.py
@@ -37,9 +37,21 @@ class BinanceFuturesClient:
                 contracts[contract_data['pair']] = contract_data
         return contracts
 
-    def get_historical_candles(self):
+    def get_historical_candles(self, symbol, interval):
+        data = dict()
+        data['symbol'] = symbol
+        data['interval'] = interval
+        data['limit'] = 1000
 
-        return
+        raw_candles = self.make_request('GET', 'fapi/v1/klines', data)
+
+        candles = []
+
+        if raw_candles is not None:
+            for candle in raw_candles:
+                candles.append([candle[0], float(candle[1]), float(candle[2]), float(candle[3]), float(candle[4]), float(candle[5])])
+
+        return candles
 
     def get_bid_ask(self, symbol):
         #'https://testnet.binancefuture.com/fapi/v1/ticker/bookTicker?symbol=BTCUSDT'  # can add parameters with "&" FOR EXAMPLE ...?symbol=BTCUSDT&key=value&key2=value2'
