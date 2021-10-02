2dbbac73dae91fa2b102cada3e5f4d6a97e31c04
prvladko <prvladko@gmail.com>
2021-10-02 11:48:07 +0300

    creates 'models' module and balance class

--- models.py
+++ models.py
@@ -1,3 +1,9 @@
 
 
-class Balance
+class Balance:
+    def __init__(self, info):
+        self.initial_margin = float(info['initialMargin'])
+        self.maintanance_margin = float(info['maintMargin'])
+        self.margin_balance = float(info['marginBalance'])
+        self.wallet_balance = float(info['walletBalance'])
+        self.unrealized_pnl = float(info['unrealizedProfit'])
