a79c5ca7b59d42755fd714283f9758c70579f27f
prvladko <prvladko@gmail.com>
2021-10-04 22:18:22 +0300

    create some styling features

--- interface/root_component.py
+++ interface/root_component.py
@@ -1,5 +1,16 @@
 import tkinter as tk
 
+from interface.styling import *
+
 class Root(tk.Tk):  # пример ООП наследования (inheritance)
     def __init__(self):
         super().__init__()
+        self.title('Trading Bot')
+
+        self.configure(bg=BG_COLOR)
+
+        self.left_frame = tk.Frame(self, bg=BG_COLOR)
+        self.left_frame.pack(side=tk.LEFT)
+
+        self.right_frame = tk.Frame(self, bg=BG_COLOR)
+        self.right_frame.pack(side=tk.LEFT)
--- main.py
+++ main.py
@@ -42,8 +42,7 @@ if __name__ == '__main__':  # statement will be executed only if the main module
     # print(bitmex.balances['XBt'].wallet_balance)  # Bitmex returns XBt (symbol of 'satoshi) instead of XBT (symbol of Bitcoin)
 
     # print(bitmex.place_order(bitmex.contracts['XBTUSD'], 'Limit', 40.4, 'Buy', 20000.49494338, 'GoodTillCancel'))
+    # print(bitmex.place_order(bitmex.contracts['XBTUSD'], 'Limit', 50, 'Buy', price=20000, tif='GoodTillCancel'))
 
-    print(bitmex.place_order(bitmex.contracts['XBTUSD'], 'Limit', 50, 'Buy', price=20000, tif='GoodTillCancel'))
-
-    root = Root  # main win dow of the bot
+    root = Root()  # main window of the bot
     root.mainloop()  # blocking func that prevent program from terminating ('event loop' func - wait for action from user)
