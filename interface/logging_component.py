22dfcc566aec0d20c6fba7de4fa3b30f57748e7a
prvladko <prvladko@gmail.com>
2021-10-04 22:35:10 +0300

    add logging component

--- interface/logging_component.py
+++ interface/logging_component.py
@@ -2,6 +2,12 @@ import tkinter as tk
 
 from interface.styling import  *
 
-class logging(tk.Frame):
-    def __init__(self):
-        super.__init__()
+class Logging(tk.Frame):
+    def __init__(self, *args, **kwargs):  # *args means that we are allowed to pass arguments without specifying its name,
+        # like we did when passing _left_frame argument  & **kwargs means that we can pass keywords arguments
+        # like bg='gray12, bg=BG_COLOR
+        super().__init__(*args, **kwargs)
+
+        self.logging_text = tk.Text(self, height=10, width=60, state=tk.DISABLED, bg=BG_COLOR, fg=FG_COLOR2,
+                                    font=GLOBAL_FONT)
+        self.logging_text.pack(side=tk.TOP)
--- interface/root_component.py
+++ interface/root_component.py
@@ -2,6 +2,8 @@ import tkinter as tk
 
 from interface.styling import *
 
+from interface.logging_component import Logging
+
 class Root(tk.Tk):  # пример ООП наследования (inheritance)
     def __init__(self):
         super().__init__()
@@ -9,8 +11,11 @@ class Root(tk.Tk):  # пример ООП наследования (inheritance)
 
         self.configure(bg=BG_COLOR)
 
-        self.left_frame = tk.Frame(self, bg=BG_COLOR)
-        self.left_frame.pack(side=tk.LEFT)
+        self._left_frame = tk.Frame(self, bg=BG_COLOR)
+        self._left_frame.pack(side=tk.LEFT)
 
-        self.right_frame = tk.Frame(self, bg=BG_COLOR)
-        self.right_frame.pack(side=tk.LEFT)
+        self._right_frame = tk.Frame(self, bg=BG_COLOR)
+        self._right_frame.pack(side=tk.LEFT)
+
+        self._logging_frame = Logging(self._left_frame, bg=BG_COLOR)
+        self._logging_frame.pack(side=tk.TOP)
