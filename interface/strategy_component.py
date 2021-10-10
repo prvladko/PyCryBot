182912ec1ba74161c3296c897f0b6f9cd100640b
prvladko <prvladko@gmail.com>
2021-10-10 10:58:48 +0300

    add button etc...

--- interface/strategy_component.py
+++ interface/strategy_component.py
@@ -3,6 +3,7 @@ import typing
 
 from interface.styling import *
 
+
 class StrategyEditor(tk.Frame):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
@@ -13,16 +14,22 @@ class StrategyEditor(tk.Frame):
         self._table_frame = tk.Frame(self, bg=BG_COLOR)
         self._table_frame.pack(side=tk.TOP)
 
+        self._add_button = tk.Button(self._commands_frame, text='Add strategy', font=GLOBAL_FONT,
+                                     command=self._add_strategy_row, bg=BG_COLOR2, fg=FG_COLOR)
+        self._add_button.pack(side=tk.TOP)
+
         self.body_widgets = dict()
 
-        self._headers = ['symbol', 'exchange', 'bid', 'ask', 'remove']
+        self._headers = ['Strategy', 'Contract', 'Timeframe', 'Balance %', 'TP %', 'SL %']
 
         for idx, h in enumerate(self._headers):
-            header = tk.Label(self._table_frame, text=h.capitalize() if h != 'remove' else '', bg=BG_COLOR,
-                              fg=FG_COLOR, font=BOLD_FONT)
+            header = tk.Label(self._table_frame, text=h, bg=BG_COLOR, fg=FG_COLOR, font=BOLD_FONT)
             header.grid(row=0, column=idx)
 
         for h in self._headers:
             self.body_widgets[h] = dict()
 
         self._body_index = 1
+
+    def _add_strategy_row(self):
+        return
