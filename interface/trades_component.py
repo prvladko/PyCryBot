37a8daabfe9823e42990e0e9af3a6e4ce2bb9d20
prvladko <prvladko@gmail.com>
2021-10-10 10:12:09 +0300

    creating the 'TradesWatch' class

---
+++ interface/trades_component.py
@@ -0,0 +1,27 @@
+import tkinter as tk
+
+from interface.styling import *
+
+
+class TradesWatch(tk.Frame):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        self._headers = ['time', 'symbol', 'exchange', 'strategy', 'side', 'quantity', 'status', 'pnl']
+
+        self._table_frame = tk.Frame(self, bg=BG_COLOR)
+        self._table_frame.pack(side=tk.TOP)
+
+        self.body_widgets = dict()
+
+        for idx, h in enumerate(self._headers):
+            header = tk.Label(self._table_frame, text=h.capitalize() if h != 'remove' else '', bg=BG_COLOR,
+                              fg=FG_COLOR, font=BOLD_FONT)
+            header.grid(row=0, column=idx)
+
+        for h in self._headers:
+            self.body_widgets[h] = dict()
+            if h in ['bid', 'ask']:
+                self.body_widgets[h + '_var'] = dict()
+
+        self._body_index = 1
