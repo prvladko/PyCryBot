b9eb71a7a8909e587bc65fb02fb9cf42d554a7ff
prvladko <prvladko@gmail.com>
2021-10-11 21:48:16 +0300

    Strategy Component_ Adding _base_params

--- interface/strategy_component.py
+++ interface/strategy_component.py
@@ -8,6 +8,9 @@ class StrategyEditor(tk.Frame):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
 
+        self._all_contracts = []
+        self._all_timeframes = ['1m', '5m', '15m', '30m', '1h', '4h']
+
         self._commands_frame = tk.Frame(self, bg=BG_COLOR)
         self._commands_frame.pack(side=tk.TOP)
 
@@ -21,6 +24,26 @@ class StrategyEditor(tk.Frame):
         self.body_widgets = dict()
 
         self._headers = ['Strategy', 'Contract', 'Timeframe', 'Balance %', 'TP %', 'SL %']
+
+        self._base_params = [
+            {'code_name': 'strategy_type', 'widget': tk.OptionMenu, 'data_type': str,
+             'values': ['Technical', 'Breakout'], 'width': 10},
+            {'code_name': 'contract', 'widget': tk.OptionMenu, 'data_type': str, 'values': self._all_contracts,
+             'width': 15},
+            {'code_name': 'timeframe', 'widget': tk.OptionMenu, 'data_type': str, 'values': self._all_timeframes,
+             'width': 7},
+            {'code_name': 'balance_pct', 'widget': tk.Entry, 'data_type': float, 'width': 7},
+            {'code_name': 'take_profit', 'widget': tk.Entry, 'data_type': float, 'width': 7},
+            {'code_name': 'stop_loss', 'widget': tk.Entry, 'data_type': float, 'width': 7},
+            {'code_name': 'balance_pct', 'widget': tk.Button, 'data_type': float, 'width': 7, 'text': 'Parameters',
+             'bg': BG_COLOR2, 'command': self._show_popup},
+            {'code_name': 'take_profit', 'widget': tk.Button, 'data_type': float, 'width': 7, 'text': 'OFF',
+             'bg': 'darkred', 'command': self._switch_strategy},
+            {'code_name': 'stop_loss', 'widget': tk.Button, 'data_type': float, 'width': 7, 'text': 'X',
+             'bg': 'darkred', 'command': self._delete_row},
+
+
+        ]
 
         for idx, h in enumerate(self._headers):
             header = tk.Label(self._table_frame, text=h, bg=BG_COLOR, fg=FG_COLOR, font=BOLD_FONT)
