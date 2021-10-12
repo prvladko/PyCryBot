79cd1dfa544b82e7a65f884fea0d458ce919d151
prvladko <prvladko@gmail.com>
2021-10-12 09:02:38 +0300

    Strategy Component_ Validating the additional parameters (still with KeyError: 'parameters')

--- interface/strategy_component.py
+++ interface/strategy_component.py
@@ -25,6 +25,9 @@ class StrategyEditor(tk.Frame):
 
         self._headers = ['Strategy', 'Contract', 'Timeframe', 'Balance %', 'TP %', 'SL %']
 
+        self._additional_parameters = dict()
+        self._extra_input = dict()
+
         self._base_params = [
             {'code_name': 'strategy_type', 'widget': tk.OptionMenu, 'data_type': str,
              'values': ['Technical', 'Breakout'], 'width': 10},
@@ -46,9 +49,9 @@ class StrategyEditor(tk.Frame):
 
         self._extra_params = {
             'Technical': [
-                {'code_name': 'ema_fast', 'name': 'MACD Fast Length', 'widget': tk.Entry, 'data': int},
-                {'code_name': 'ema_slow', 'name': 'MACD Slow Length', 'widget': tk.Entry, 'data': int},
-                {'code_name': 'ema_signal', 'name': 'MACD Signal Length', 'widget': tk.Entry, 'data': int},
+                {'code_name': 'ema_fast', 'name': 'MACD Fast Length', 'widget': tk.Entry, 'data_type': int},
+                {'code_name': 'ema_slow', 'name': 'MACD Slow Length', 'widget': tk.Entry, 'data_type': int},
+                {'code_name': 'ema_signal', 'name': 'MACD Signal Length', 'widget': tk.Entry, 'data_type': int},
             ],
             'Breakout': [
                 {'code_name': 'min_volume', 'name': 'Minimum Volume', 'widget': tk.Entry, 'data': float}
@@ -92,6 +95,12 @@ class StrategyEditor(tk.Frame):
 
             self.body_widgets[code_name][b_index].grid(row=b_index, column=col)
 
+        self._additional_parameters[b_index] = dict()
+
+        for strat, params in self._extra_params.items():
+            for param in params:
+                self._additional_parameters[b_index][param['code_name']] = None
+
         self._body_index += 1
 
     def _delete_row(self, b_index: int):
@@ -126,10 +135,12 @@ class StrategyEditor(tk.Frame):
             temp_label.grid(row=row_nb, column=0)
 
             if param['widget'] == tk.Entry:
-                temp_input = tk.Entry(self._popup_window, bg=BG_COLOR2, justify=tk.CENTER, fg=FG_COLOR,
+                self._extra_input[code_name] = tk.Entry(self._popup_window, bg=BG_COLOR2, justify=tk.CENTER, fg=FG_COLOR,
                                       insertbackground=FG_COLOR)
             else:
                 continue
+
+            self._extra_input[code_name].grid(row=row_nb, column=1)
 
             row_nb += 1
 
@@ -140,7 +151,19 @@ class StrategyEditor(tk.Frame):
         validation_button.grid(row=row_nb, column=0, columnspan=2)
 
     def _validate_parameters(self, b_index: int):
-        return
+
+        strat_selected = self.body_widgets['strategy_type_var'][b_index].get()
+
+        for param in self._extra_params[strat_selected]:
+            code_name = param['code_name']
+
+            if self._extra_input[code_name].get() == '':
+                self._additional_parameters[code_name] = None
+            else:
+                self._additional_parameters[code_name] = param['data_type'](self._extra_input[code_name].get())
+                # ^^^ can be str, float, int ^^^
+
+        self._popup_window.destroy()
 
     def _switch_strategy(self, b_index: int):
         return
