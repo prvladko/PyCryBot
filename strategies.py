c23a19a4b80af1927635be7ee74357f59087d44d
prvladko <prvladko@gmail.com>
2021-10-17 20:25:23 +0300

    create instance variables for each of attributes

--- strategies.py
+++ strategies.py
@@ -8,3 +8,10 @@ logger = logging.getLogger()
 class Strategy:
     def __init__(self, contract: Contract, exchange: str, timeframe: str, balance_pct: float, take_profit: float,
                  stop_loss: float):  # tp & sl in percentage
+
+        self.contract = contract
+        self.exchange = exchange
+        self.tf = timeframe
+        self.balance_pct = balance_pct
+        self.take_profit = take_profit
+        self.stop_loss = stop_loss
