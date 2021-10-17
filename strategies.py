89c9ed10fe43b4258e33983c6f66b6de4cd5cb8f
prvladko <prvladko@gmail.com>
2021-10-17 20:33:18 +0300

    add 'TechnicalStrategy' class

--- strategies.py
+++ strategies.py
@@ -1,4 +1,5 @@
 import logging
+import typing
 
 from models import *
 
@@ -19,4 +20,9 @@ class Strategy:
 
 class TechnicalStrategy(Strategy):
     def __init__(self, contract: Contract, exchange: str, timeframe: str, balance_pct: float, take_profit: float,
-                 stop_loss: float, other_params):  # other_params for example ema_fast
+                 stop_loss: float, other_params: typing.Dict):  # other_params for example ema_fast
+        super().__init__(contract, exchange, timeframe, balance_pct, take_profit, stop_loss)
+
+        self._ema_fast = other_params['ema_fast']
+        self._ema_slow = other_params['ema_slow']
+        self._ema_signal = other_params['ema_signal']
