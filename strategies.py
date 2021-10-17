7fae00213489bbf68ef428d4214161635f839afe
prvladko <prvladko@gmail.com>
2021-10-17 20:27:38 +0300

    add 'TechnicalStrategy' class

--- strategies.py
+++ strategies.py
@@ -15,3 +15,8 @@ class Strategy:
         self.balance_pct = balance_pct
         self.take_profit = take_profit
         self.stop_loss = stop_loss
+
+
+class TechnicalStrategy(Strategy):
+    def __init__(self, contract: Contract, exchange: str, timeframe: str, balance_pct: float, take_profit: float,
+                 stop_loss: float, other_params):  # other_params for example ema_fast
